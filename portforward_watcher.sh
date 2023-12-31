9d4648a4d0d9a367b63e2f4dd77f25ba9195489b
biskyt <biskyt@users.noreply.github.com>
2023-12-31 13:55:14 +0000

    tidy script

--- portforward_watcher.sh
+++ portforward_watcher.sh
@@ -5,14 +5,16 @@ sleep 10 # give transmission time to start
 PORT=$(cat /portforward/port.dat | sed 's/[[:space:]]//g')
 transmission_host=${TRHOST:="localhost"}
 transmission_port=${TRPORT:="9091"}
-transmission_url="http://${TRHOST}:${TRPORT}/transmission"
+transmission_url="http://${transmission_host}:${transmission_port}/transmission"
 echo "Update port forward to: ${PORT} for ${transmission_url}"
-transmission-remote ${transmission_url} -n ${TRUSER}:${TRPASSWORD} -p ${PORT}
+transmission-remote "${transmission_url}" -n "${TRUSER}:${TRPASSWORD}" -p "${PORT}"
+echo "result: $?"
 
 inotifywait -q -m -e close_write /portforward/port.dat |
-while read -r filename event; do
-  PORT=`cat /portforward/port.dat`
-  echo "port.dat has been updated."
-  echo "Update port forward to: ${PORT} for ${transmission_url}"
-  transmission-remote ${transmission_url} -n ${TRUSER}:${TRPASSWORD} -p ${PORT}
-done
+  while read -r; do
+    PORT=$(cat /portforward/port.dat)
+    echo "port.dat has been updated."
+    echo "Update port forward to: ${PORT} for ${transmission_url}"
+    transmission-remote "${transmission_url}" -n "${TRUSER}:${TRPASSWORD}" -p "${PORT}"
+    echo "result: $?"
+  done
